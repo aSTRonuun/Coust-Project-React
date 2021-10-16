ffc8ab088ddda95cc5eae4435da0e2b9118a590c
aSTRonuun <vitormanoel00710@gmail.com>
2021-10-16 13:12:08 -0300

    backend e api adicionados

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import Submit from '../form/Submit'
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'aplication/JSON',
+            },
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input 
@@ -21,6 +40,7 @@ function ProjectForm({btnText}) {
             <Select
                 name="catagory_id"
                 text="Selecione uma categoria"
+                options={categories}
             />
             <Submit text={btnText}/>
         </form>
